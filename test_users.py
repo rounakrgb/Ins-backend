13d107f7ba416e8199539e3f53171647ff2b768d
rounak <rounakmalik168@gmail.com>
2026-03-04 19:49:22 +0530

    testfile_changed

--- test_users.py
+++ test_users.py
@@ -5,11 +5,11 @@ client = TestClient(app)
 
 def test_add_user():
     response = client.post(
-        "/signup",
-        params = {
-            "username":"TestUser",
-            "password":"any@1343"
-        }
+    "/signup",
+    json={
+        "username": "TestUser",
+        "password": "any@1343"
+    }
     )
     
     assert response.status_code == 200
